f2c91662747bf1ee322d7aeeca42b9e6b516187d
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-02-15 18:40:55 -0700

    added get messages

--- Data/PublicDal.cs
+++ Data/PublicDal.cs
@@ -1,4 +1,5 @@
-﻿using UniversalChatRoom.Interfaces;
+﻿using Humanizer.Localisation;
+using UniversalChatRoom.Interfaces;
 using UniversalChatRoom.Models;
 
 namespace UniversalChatRoom.Data {
@@ -21,6 +22,22 @@ namespace UniversalChatRoom.Data {
 		public static void AddProfile(Profile profile) {
 			//db.Profiles.Add(profile);
 			//db.SaveChanges();
+		}
+
+		public IEnumerable<Message> getMessages(Chatroom? chatroom) {
+			if (chatroom == null) {
+				chatroom = db.Chatrooms.First();
+			}
+			IEnumerable<ChatroomMessage> messagesinChat = db.ChatroomMessages.Where(m => m.ChatroomID == chatroom.ID ).ToList();
+			List<Message> messages = new List<Message>();
+			foreach (ChatroomMessage message in messagesinChat) {
+				messages.Add(db.Messages.Where(m => m.ID == message.MessageID).First());	
+			}
+
+			//sort message by date.
+
+			return messages;
+
 		}
 	}
 }
